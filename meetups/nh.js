86070e71cee85f2d79b64f09fa49c8e22346e79d
Linda Zanchi <linda.a.zanchi@gmail.com>
2019-10-14 20:03:29 -0400

    Create nh.js

---
+++ meetups/nh.js
@@ -0,0 +1,13 @@
+MEETUP_NAME: nh.js: New Hampshire's JavaScript User Group
+
+LINK_TO_MEETUP_WEBSITE: [nh.js](https://www.meetup.com/nhjs-meetup/)
+
+MEETUP_DATE: Next Meetup on November 13, 2019 6:00 p.m.
+
+MEETUP_TYPE: PHYSICAL
+
+MEETUP_LOCATION: 175 Canal Street, Manchester, NH USA
+
+LAST_APPLICATION_DATETIME: October 9, 2019
+
+SPONSOR: [Alexander Technology Group](https://www.bankwstaffing.com/alexandertg/)
